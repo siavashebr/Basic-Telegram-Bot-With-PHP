01e415140487d6eabede497cf52ab4fdcf355326
Mahdi <130539341+mhdi-khosravi@users.noreply.github.com>
2024-05-03 12:18:49 +0330

    Add BlackList and Ignore Channels Updates

--- main/bot.php
+++ main/bot.php
@@ -3,6 +3,7 @@ namespace bot\main\bot;
 
 fastcgi_finish_request();
 
+use bot\generalDBFunctions\generalDBFunctions;
 use bot\generalFunctions\generalFunctions;
 use bot\admin\adminBot\adminBot;
 use bot\user\userBot\userBot;
@@ -13,16 +14,46 @@ require_once __DIR__ . '/../admin/adminBot.php';
 require_once __DIR__ . '/../user/userBot.php';
 
 define('generalFunctions', new generalFunctions());
+define('database', new generalDBFunctions());
+
 define('adminBot', new adminBot());
 define('userBot', new userBot());
 
 if(updateType!= NULL)
 {
-    if(generalFunctions->isNewUser())
+
+    if(updateType == "callback_query")
+    {
+        bot->answerCallbackQuery(
+            [
+                'callback_query_id' => callback_id
+            ]
+        );
+    }
+    if(generalFunctions->isNewUser() and !str_starts_with(chatId,'-'))
     {
         generalFunctions->addStep();
     }
-    if(generalFunctions->isAdmin())
+    if(str_starts_with(chatId,'-'))
+    {
+        return;
+    }
+    else if(generalFunctions->isBlackList(chatId))
+    {
+        generalFunctions->sendMessage('شما در لیست سیاه قرار دارید');
+    }
+    if(callback_data == 'back' or userText == 'بازگشت')
+    {
+        if(generalFunctions->isAdmin())
+        {
+            adminBot->mainMenu();
+        }
+        else
+        {
+            userBot->mainMenu();
+        }
+    }
+    else if(generalFunctions->isAdmin())
     {
         adminBot->main();
     }
